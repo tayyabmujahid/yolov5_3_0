f15281586f4fb603fe3922fa0f9bee49fd0e4b5c
TM_workstation <tayyab.mujahid@gmail.com>
2021-05-20 18:40:47 +0300

    added entity

--- utils/wandb_logging/wandb_utils.py
+++ utils/wandb_logging/wandb_utils.py
@@ -94,6 +94,7 @@ class WandbLogger():
         elif self.wandb:
             self.wandb_run = wandb.init(config=opt,
                                         resume="allow",
+                                        entity ="ksa_flyer",
                                         project='cropping' if opt.project == 'runs/train' else Path(opt.project).stem,
                                         name=name,
                                         job_type=job_type,
